6c59f5e0d0f4c7e3cae9db5ff6aeb6bd8766e99a
Siegue, Jason Wowie D. Jr <166314135+wowie123123@users.noreply.github.com>
2025-07-12 13:04:07 +0800

    Add files via upload

---
+++ SIEGUE_PROG_1/SIEGUE_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+class SIEGUE_ACTIVITY_I {
+    public static void main(String[] args) {
+        double a = 10.5;
+        double b = 4.5;
+        double c = 3.0;
+        double d = 2.5;
+        double e = 1.0;
+        double f = 6.0;
+        double g = 2.0;
+
+        double addition = a + b + c;                  // wowie gwapo
+        double subtraction = d - e;               
+        double multiplication = f * g;                  
+        double division = a / g;                 
+        System.out.println("addition = " + addition);
+        System.out.println("subtraction = " + subtraction);
+        System.out.println("multiplication = " + multiplication);
+        System.out.println("divide = " + division);
+    }
+}

db1a3bf214105f43404a6ec584518bad3c9fffac
Siegue, Jason Wowie D. Jr <166314135+wowie123123@users.noreply.github.com>
2025-07-13 17:36:31 +0800

    Add files via upload

---
+++ SIEGUE_PROG_1/SIEGUE_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+class SIEGUE_ACTIVITY_I {
+    public static void main(String[] args) {
+        
+        double x = 5.2;
+        double y = 4.6;
+        double z = 3.8;
+        double sum = x + y + z;   
+        double difference = y - z;      
+        double product = x * z;
+        double quotient = y / z;
+        
+        System.out.println("sum = " + sum);
+        System.out.println("difference = " + difference);
+        System.out.println("product = " + product);
+        System.out.println("quotient = " + quotient);
+    }
+}
